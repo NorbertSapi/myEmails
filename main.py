5d77974f2a8b8dfa4f88c8ad3a5e31b16c224064
Norbert Sapi <ncs.sapi@gmail.com>
2023-05-11 10:36:09 +0100

    add resources directory, and add messages and names txt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+"""
 x = open("Hello.txt", "r")
 y = x.read()
 print(y)
@@ -6,3 +7,13 @@ x.close()
 
 with open("Hello.txt", "w") as file:
     file.write("Hey there!")
+"""
+
+with open("Names.txt", "r") as name_file:
+    with open("README.txt", "r") as message_file:
+        body = message_file.read()
+        for name in name_file:
+
+            mail = "Hello " + name + body
+            with open(name.strip() + ".txt", + ".txt", 'w') as message_file:
+                message_file(mail)
